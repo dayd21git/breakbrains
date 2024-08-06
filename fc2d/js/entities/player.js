5eb0d8b2c74329f62d815e25ef4d11d9d2be1f3b
day-d <61166549+dayd21git@users.noreply.github.com>
2024-08-06 19:07:27 +0400

    Add files via upload

---
+++ fc2d/js/entities/player.js
@@ -0,0 +1,57 @@
+class Player extends EntityBase {
+    constructor(name, coords, scale, health, speed) {
+        super(name, coords, scale);
+        this.name = name;
+        this.position = coords; // [x, y, z]
+        this.scale = scale;
+        this.health = health;
+        this.speed = speed;
+
+        // Create the player mesh (e.g., a sphere)
+        this.mesh = BABYLON.MeshBuilder.CreateSphere('player', { diameter: 1 }, scene);
+        this.mesh.position = new BABYLON.Vector3(this.position[0], this.position[1], this.position[2]);
+
+        console.log(`Player mesh created at ${this.mesh.position.toString()}`);
+    }
+
+    // Method to move the player
+    move(direction) {
+        const speed = this.speed / 100;
+        switch (direction) {
+            case 'left':
+                this.position[0] = parseFloat((this.position[0] - speed).toFixed(2));
+                break;
+            case 'right':
+                this.position[0] = parseFloat((this.position[0] + speed).toFixed(2));
+                break;
+            case 'up':
+                this.position[2] = parseFloat((this.position[2] - speed).toFixed(2));
+                break;
+            case 'down':
+                this.position[2] = parseFloat((this.position[2] + speed).toFixed(2));
+                break;
+        }
+        this.mesh.position = new BABYLON.Vector3(this.position[0], this.position[1], this.position[2]);
+        console.log(`Player moved to ${this.mesh.position.toString()}`);
+    }
+
+    getPosition() {
+        return this.position;
+    }
+
+    setHealth(health) {
+        this.health = health;
+    }
+
+    getHealth() {
+        return this.health;
+    }
+
+    setSpeed(speed) {
+        this.speed = speed;
+    }
+
+    getSpeed() {
+        return this.speed;
+    }
+}
