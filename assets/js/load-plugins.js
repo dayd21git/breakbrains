543adb3f369cc237307f3f867b76a5b2006d73ee
day-d <61166549+dayd21git@users.noreply.github.com>
2024-09-01 06:13:09 +0400

    Update load-plugins.js

--- assets/js/load-plugins.js
+++ assets/js/load-plugins.js
@@ -1,18 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
     fetch('plugins.json')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             const listElement = document.getElementById('list');
-            const template = document.getElementById('item-template').innerHTML;
+            if (!listElement) {
+                console.error('Element with id "list" not found');
+                return;
+            }
+
+            const template = document.getElementById('item-template');
+            if (!template) {
+                console.error('Template with id "item-template" not found');
+                return;
+            }
+
+            const templateContent = template.innerHTML;
 
             // Пройдемся по каждому ключу в JSON
             for (const key in data) {
                 if (data.hasOwnProperty(key)) {
                     const item = data[key];
-                    let itemHtml = template
+                    let itemHtml = templateContent
                         .replace(/{KEY}/g, key)
                         .replace(/{NAME}/g, item.name)
-                        .replace(/{DESCRIPTION}/g, item.description);
+                        .replace(/{DESCRIPTION}/g, item.Description);
 
                     // Создаем новый элемент и добавляем его в список
                     const itemElement = document.createElement('div');
