09265899bfcbce5d5d2ab2d6c1dc4c3ce1d06c3f
day-d <61166549+dayd21git@users.noreply.github.com>
2024-09-01 05:35:23 +0400

    Update load-items.js

--- assets/js/load-items.js
+++ assets/js/load-items.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('data.json')
+    fetch('plugins.json')
         .then(response => response.json())
         .then(data => {
             const listElement = document.getElementById('list');
